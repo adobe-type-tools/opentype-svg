4410a5a34b5c9de73b9e675286ad804cef795c11
Miguel Sousa <msousa@adobe.com>
2020-05-01 11:06:23 -0700

    Package the scripts. Improve documentation.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="opentypesvg",
+    version="1.1.1",
+    author="Miguel Sousa",
+    author_email="msousa@adobe.com",
+    description="Tools for making OpenType-SVG fonts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adobe-type-tools/opentype-svg",
+    license="MIT",
+    platforms=["Any"],
+    package_dir={'': 'lib'},
+    packages=['opentypesvg'],
+    python_requires='>=3.6',
+    install_requires=['fontTools[woff]>=3.1.0'],
+    entry_points={
+        'console_scripts': [
+            "addsvg = opentypesvg.addsvg:main",
+            "dumpsvg = opentypesvg.dumpsvg:main",
+            "fonts2svg = opentypesvg.fonts2svg:main",
+        ]
+    },
+)
